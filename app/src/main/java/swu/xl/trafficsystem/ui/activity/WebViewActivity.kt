fd8a70564e61ce47cf13ea3e63fef88a45b5b4cd
xulei <2867584387@qq.com>
2021-04-29 17:53:15 +0800

    用户协议部分完成

---
+++ app/src/main/java/swu/xl/trafficsystem/ui/activity/WebViewActivity.kt
@@ -0,0 +1,71 @@
+package swu.xl.trafficsystem.ui.activity
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import android.view.View
+import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_webview.*
+import swu.xl.trafficsystem.R
+import swu.xl.trafficsystem.base.BaseActivity
+
+class WebViewActivity : BaseActivity() {
+    companion object {
+        private const val URL = "SecondHouseURL"
+
+        fun loadURL(context: Context, url: String) {
+            val intent = Intent(context, WebViewActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            intent.putExtra(URL, url)
+            context.startActivity(intent)
+        }
+    }
+
+    override fun preInit() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+                    )
+            window.statusBarColor = Color.TRANSPARENT
+            window.navigationBarColor = Color.TRANSPARENT
+        }
+    }
+
+    override fun getLayoutId() = R.layout.activity_webview
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun initData() {
+        val url = intent.getStringExtra(URL)
+        web_view.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                return false
+            }
+        }
+        web_view.settings.apply {
+            javaScriptEnabled = true
+            javaScriptCanOpenWindowsAutomatically = true
+            setAppCacheEnabled(true)
+            loadWithOverviewMode = true
+            setSupportZoom(true)
+            useWideViewPort = true
+            displayZoomControls = true
+            layoutAlgorithm = WebSettings.LayoutAlgorithm.NORMAL
+        }
+        web_view.loadUrl(url)
+    }
+
+    override fun onBackPressed() {
+        if (web_view.canGoBack()) {
+            web_view.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
--- app/src/main/java/swu/xl/trafficsystem/ui/fragment/UserFragment.kt
+++ app/src/main/java/swu/xl/trafficsystem/ui/fragment/UserFragment.kt
@@ -1,11 +1,21 @@
 package swu.xl.trafficsystem.ui.fragment
 
 import android.view.View
+import kotlinx.android.synthetic.main.fragment_user.*
 import swu.xl.trafficsystem.R
 import swu.xl.trafficsystem.base.BaseFragment
+import swu.xl.trafficsystem.ui.activity.WebViewActivity
 
 class UserFragment: BaseFragment() {
     override fun initView(): View {
         return View.inflate(context, R.layout.fragment_user, null)
+    }
+
+    override fun initListener() {
+        user_document.setOnClickListener {
+            activity?.let {
+                WebViewActivity.loadURL(it, "https://cache.amap.com/h5/h5/publish/238/index.html")
+            }
+        }
     }
 }
