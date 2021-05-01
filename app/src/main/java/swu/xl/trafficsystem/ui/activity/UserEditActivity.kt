4a857d01e5cf7d5b31a36685c7b311b44f7dadf2
xulei <2867584387@qq.com>
2021-05-01 11:04:26 +0800

    个人资料初始化部分完成

--- app/src/main/java/swu/xl/trafficsystem/ui/activity/UserEditActivity.kt
+++ app/src/main/java/swu/xl/trafficsystem/ui/activity/UserEditActivity.kt
@@ -2,8 +2,10 @@ package swu.xl.trafficsystem.ui.activity
 
 import android.content.Context
 import android.content.Intent
+import kotlinx.android.synthetic.main.activity_user_edit.*
 import swu.xl.trafficsystem.R
 import swu.xl.trafficsystem.base.BaseActivity
+import swu.xl.trafficsystem.manager.UserManager
 
 class UserEditActivity : BaseActivity() {
     companion object {
@@ -15,4 +17,20 @@ class UserEditActivity : BaseActivity() {
     }
 
     override fun getLayoutId() = R.layout.activity_user_edit
+
+    override fun initData() {
+        UserManager.getCurrentUser()?.let {
+            it.picture?.let { }
+            user_nick_name.text = it.nickname
+            user_password.text = getPassword(it.password)
+        }
+    }
+
+    private fun getPassword(password: String): String {
+        var length = ""
+        repeat(password.length) {
+            length += "*"
+        }
+        return length
+    }
 }
